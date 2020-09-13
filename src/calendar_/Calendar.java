d53983da792f03b0804d291da61a5027140b58c6
jini0201 <xinatte@gmail.com>
2020-09-13 13:35:01 +0900

    Update repeat function

--- src/calendar_/Calendar.java
+++ src/calendar_/Calendar.java
@@ -10,31 +10,23 @@ public class Calendar {
 		return MAX_DAYS[month-1];
 	}
 	
-	public void printSampleCalendar() {
-		System.out.println("일  월  화  수  목  금  토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-	
 	public static void main(String[] args) {
 
-
-		
-		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		System.out.println("달을 입력하시오.");
 		Calendar cal = new Calendar();
+		
+		System.out.println("반복횟수를 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
-		int month = scanner.nextInt();
+		int count = scanner.nextInt();
+		int[] number = new int[12];
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		System.out.println("월을 입력하세요.");
+		for(int i=0; i<count; i++) {
+			number[i] = scanner.nextInt();
+		}
 		
-		cal.printSampleCalendar();
-		
-		scanner.close();
-		
+		for(int i=0; i<count; i++) {
+			System.out.printf("%d월은 %d일까지 있습니다.\n", number[i], cal.getMaxDaysOfMonth(number[i]));
+		}
 		
 	}
 
